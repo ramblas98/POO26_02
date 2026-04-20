d6769f78ad266d68d4fc81b94873476c9e9545c0
Jose David <asefadoj@gmail.com>
2026-04-20 18:54:26 -0300

    feat(unsa) add ejercicio0 test

---
+++ TP0_EJ0/src/main/java/ar/edu/unsa/tp0_ej0/TP0_EJ0.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ar.edu.unsa.tp0_ej0;
+
+/**
+ *
+ * @author jd
+ */
+public class TP0_EJ0 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

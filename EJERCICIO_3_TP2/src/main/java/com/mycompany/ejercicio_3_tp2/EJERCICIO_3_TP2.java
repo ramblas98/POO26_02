4dd10c72a7e14ddbd3601b631c39af99a6ae7227
Agustin Figueroa <gucho567@gmail.com>
2026-04-25 21:06:23 -0300

    ejercicio 3 racionales PC

---
+++ EJERCICIO_3_TP2/src/main/java/com/mycompany/ejercicio_3_tp2/EJERCICIO_3_TP2.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio_3_tp2;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class EJERCICIO_3_TP2 {
+
+    public static void main(String[] args) {
+        Racional r1,r2;
+
+        r1 = new Racional();
+        r2 = new Racional();
+        System.out.println(r1);
+        System.out.println(r2);
+
+
+        System.out.println("--------------");
+
+        r1 = new Racional(3,6);
+        r1.simplificar();
+        System.out.println(r1);
+        System.out.println("--------------");
+
+        r2 = new Racional(5,10);
+        r2.simplificar();
+        System.out.println(r2);
+
+        Racional r3 = r1;
+        System.out.println("--------------");
+        if(r1.equals(r2)){
+            System.out.println("Son iguales");
+        }else{
+            System.out.println("No son iguales");
+        }
+        System.out.println("--------------");
+
+        System.out.println(r1);
+
+        System.out.println("Pedir numerador usando Getter:");
+        int numer = r1.getNumerador();
+        System.out.println(numer);
+
+        System.out.println("Pedir Denominador usando Getter:");
+        int denom = r1.getDenominador();
+        System.out.println(denom);
+
+        System.out.println("--------------");
+
+        System.out.println(r2);
+        System.out.println("Cambiar numerador y denominador con Setter:");
+        r2.setNumerador(2);
+
+        r2.setDenominador(5);
+        System.out.println(r2);
+
+        System.out.println("--------------");
+        System.out.println("Metodos de instancia:");
+
+        Racional temp = new Racional(3,8);
+        temp.simplificar();
+
+        Racional temp2 = new Racional(5,10);
+        temp2.simplificar();
+
+        temp.sumar(temp2);
+        System.out.println(temp);
+
+        temp.restar(temp2);
+        System.out.println(temp);
+
+        temp.multiplicar(temp2);
+        System.out.println(temp);
+
+        temp.dividir(temp2);
+        System.out.println(temp);
+
+        System.out.println("--------------");
+        System.out.println(r1 + " a decimal -> " + r1.aDecimal());
+        System.out.println("--------------");
+        System.out.println(r1.compareTo(r2));
+        System.out.println("--------------");
+
+        Racional suma = Racional.sumar(r1,r2);
+        suma.simplificar();
+        System.out.println(suma);
+        System.out.println(r1 + "+" + r2 + "=" + suma);
+        System.out.println("--------------");
+
+        Racional resta = Racional.restar(r1,r2);
+        resta.simplificar();
+        System.out.println(resta);
+        System.out.println(r1 + "-" + r2 + "=" + resta);
+        System.out.println("--------------");
+
+        Racional multi = Racional.multiplicar(r1,r2);
+        multi.simplificar();
+        System.out.println(multi);
+        System.out.println(r1 + " * " + r2 + "=" + multi);
+        System.out.println("--------------");
+
+        Racional divi = Racional.dividir(r1,r2);
+        divi.simplificar();
+        System.out.println(divi);
+        System.out.println(r1 + " / " + r2 + "=" + divi);
+        System.out.println("--------------");
+    }
+}
---
+++ EJERCICIO_3_TP2/src/main/java/com/mycompany/ejercicio_3_tp2/Racional.java
@@ -0,0 +1,148 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ejercicio_3_tp2;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Racional {
+    private int numerador;
+    private int denominador;
+
+    //Inicializar Racional xd
+    public Racional(){
+        this.numerador = 0;
+        this.denominador = 1;
+    }
+
+    //Pedir Racional
+    public Racional(int numerador, int denominador){
+        this.numerador = numerador;
+        if(denominador == 0){
+            this.denominador = 1;
+            System.out.println("Tiene que ser distinto de 0 pero se agrego 1 por defecto");
+        }
+        this.denominador = denominador;
+    }
+
+    public int getNumerador() {
+        return numerador;
+    }
+
+    public int getDenominador() {
+        return denominador;
+    }
+
+    public void setNumerador(int numerador) {
+        this.numerador = numerador;
+    }
+
+    public void setDenominador(int denominador) {
+        if(denominador != 0){
+            this.denominador = denominador;
+        }else{
+            System.out.println("Denominador distinto de 0");
+        }
+    }
+
+    public void sumar(Racional r){
+        this.numerador = this.numerador * r.denominador + r.numerador * this.denominador;
+        this.denominador = r.denominador * this.denominador;
+    }
+
+    public void restar(Racional r){
+        this.numerador = this.numerador * r.denominador - r.numerador * this.denominador;
+        this.denominador = r.denominador * this.denominador;
+    }
+
+    public void multiplicar(Racional r){
+        this.numerador = r.numerador * this.numerador;
+        this.denominador = r.denominador * this.denominador;
+    }
+
+    public void dividir(Racional r){
+        this.numerador = this.numerador * r.denominador;
+        this.denominador = this.denominador * r.numerador;
+    }
+
+    public Racional simplificar(){
+        int divisor = mcd(Math.abs(numerador), Math.abs(denominador));
+
+        numerador /= divisor;
+        denominador /= divisor;
+
+        if (denominador < 0) {
+            numerador *= -1;
+            denominador *= -1;
+        }
+
+        return this;
+    }
+
+    public double aDecimal(){
+        return (double) numerador/denominador;
+    }
+
+    @Override
+    public String toString() {
+        return numerador + "/" + denominador;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Racional){
+            Racional otro = (Racional) obj;
+
+            return this.numerador * otro.denominador == otro.numerador * this.denominador;
+        }
+
+        return false;
+    }
+
+    public int compareTo(Racional r) {
+        int valor1 = this.numerador * r.denominador;
+        int valor2 = r.numerador * this.denominador;
+
+        return Integer.compare(valor1,valor2);
+    }
+
+    public static Racional sumar(Racional r1,Racional r2){
+        int nume = r1.numerador * r2.denominador + r2.numerador * r1.denominador;
+        int demo = r1.denominador * r2.denominador;
+
+        return new Racional(nume,demo);
+    }
+
+    public static Racional restar(Racional r1,Racional r2){
+        int nume = r1.numerador * r2.denominador - r2.numerador * r1.denominador;
+        int demo = r1.denominador * r2.denominador;
+
+        return new Racional(nume,demo);
+    }
+
+    public static Racional multiplicar(Racional r1,Racional r2){
+        int nume = r1.numerador * r2.numerador;
+        int demo = r1.denominador * r2.denominador;
+
+        return new Racional(nume,demo);
+    }
+
+    public static Racional dividir(Racional r1,Racional r2){
+        int nume = r1.numerador * r2.denominador;
+        int demo = r1.denominador * r2.numerador;
+
+        return new Racional(nume,demo);
+    }
+
+    private static int mcd(int a, int b){
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}

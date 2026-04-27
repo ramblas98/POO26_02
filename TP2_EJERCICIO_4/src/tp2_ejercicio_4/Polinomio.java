dc97cea5a7b52d1bf1fe19a1027829970a173051
Pc <Pc@192.168.0.102>
2026-04-27 16:01:52 -0300

    primer codigo

---
+++ TP2_EJERCICIO_4/src/tp2_ejercicio_4/Polinomio.java
@@ -0,0 +1,442 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_ejercicio_4;
+
+/**
+ *
+ * @author Pc
+ */
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Polinomio {
+    private int grado;
+    private double[] coeficientes;
+    
+
+    public Polinomio(){
+        System.out.println("Obj. Polinomio creado.");
+    }
+
+    //Constructor que recibe un arreglo de coeficientes
+    public Polinomio(double[] coeficientesRecibidos){ 
+        coeficientes = coeficientesRecibidos;
+    }
+    
+    //Constructor que recibe un grado y crea un polinomio con todos los coeficientes
+    public Polinomio(int gradoRecibido){
+        coeficientes = new double[gradoRecibido +1];   
+    }
+    
+    //Métodos de instancia para operaciones básicas que modifican los atributos del Polinomio:
+    //sumar(Polinomio p): Suma dos polinomios
+    public void sumar(Polinomio p){ //Metodo de instancia
+        int gradoS,i;
+        double coef1, coef2;
+        
+        if(this.grado > p.grado){
+            gradoS = this.grado;
+        } else gradoS = p.grado;
+        
+        Polinomio suma = new Polinomio();
+        suma.grado= gradoS;
+        suma.coeficientes= new double[gradoS +1];
+        
+          for(i=0; i<= gradoS; i++){
+              coef1=0;
+              coef2=0;
+          
+          if( i<= this.grado){
+              coef1 = this.coeficientes[i];
+          }
+          if(i <= p.grado){
+              coef2= p.coeficientes[i];
+          }
+          
+            suma.coeficientes[i] = coef1 + coef2;
+          }  
+          this.grado= suma.grado;
+          this.coeficientes = suma.coeficientes; 
+    }
+    
+    //restar(Polinomio p): Resta un polinomio del actual
+    public void restar(Polinomio q){
+        int gradoR,i;
+        double coef1, coef2;
+        
+        if(this.grado > q.grado){
+            gradoR = this.grado;
+        } else gradoR = q.grado;
+        
+        Polinomio resta = new Polinomio();
+        resta.grado= gradoR;
+        resta.coeficientes= new double[gradoR +1];
+        
+          for(i=0; i<= gradoR; i++){
+              coef1=0;
+              coef2=0;
+          
+          if( i<= this.grado){
+              coef1 = this.coeficientes[i];
+          }
+          if(i <= q.grado){
+              coef2= q.coeficientes[i];
+          }
+          
+            resta.coeficientes[i] = coef1 - coef2;
+          }  
+          this.grado= resta.grado;
+          this.coeficientes = resta.coeficientes; 
+    }
+       
+
+    
+    // multiplicar(Polinomio p): Multiplica dos polinomios
+    public void multiplicar(Polinomio m){
+        int gradoM = this.grado + m.getGrado();
+        double[] nuevoArreglo = new double[gradoM +1];
+        
+       for(int i=0; i<= this.grado; i++){
+           for(int j=0; j<= m.grado ;j++){
+               nuevoArreglo[i+j] += m.coeficientes[j] * this.coeficientes[i];  
+           }
+       }
+      
+       this.coeficientes = nuevoArreglo;
+    }
+    
+    
+    public int getGrado(){
+    return grado;
+}
+
+public double[] getCoeficientes(){
+    return coeficientes.clone();
+}
+
+public double getCoeficiente(int indice){
+    if(indice >= 0 && indice < coeficientes.length){
+        return coeficientes[indice];
+    } else{
+    System.out.println("Indice fuera de rango");
+    }
+    return 0;
+}
+
+public void setCoeficiente(int indice, double nuevoCoef){
+    if(indice >this.grado || indice < 0){
+        System.out.println("Indice no valido.Ingresar un valor entre 0 y " + this.grado);
+    } else {
+        this.coeficientes[indice] = nuevoCoef;
+    }
+}
+
+public void setGrado(int nuevoGrado){
+    if (nuevoGrado >= coeficientes.length) {
+        System.out.println("Error: El grado excede la capacidad del arreglo.");
+        return;
+    }
+    // 2. Limpiar (poner en 0) todos los términos que quedan "fuera" del nuevo grado
+    for (int i = nuevoGrado + 1; i <= this.grado; i++) {
+        this.coeficientes[i] = 0;
+    }
+    this.grado = nuevoGrado;
+}
+
+public void ingresarPolinomio(){
+    Scanner teclado = new Scanner(System.in);
+    
+    System.out.println("ingresar grado: ");
+    this.grado = teclado.nextInt();
+    teclado.nextLine();
+    this.coeficientes = new double[this.grado +1];
+
+    for(int i=grado ; i >= 0 ; i--){
+        double valor;
+        System.out.println("Ingrese coeficiente para X^" + i + ": ");
+        valor = teclado.nextDouble();
+        
+        while(i== this.grado && valor == 0){
+            System.out.println("Ingrese coeficiente para X^" + i + ": ");
+            System.out.println("no puede valer 0");
+            valor = teclado.nextDouble();
+        }
+    this.coeficientes[i] = valor;
+    }
+}
+
+public void agregarCoeficiente(double coef, int exp){
+    
+    if(exp > this.grado){
+        double[] nuevoArreglo= new double[exp +1];
+        
+        
+        nuevoArreglo[exp]= coef;
+        for(int i= 0; i<=this.grado; i++){
+            nuevoArreglo[i] = coeficientes[i];
+        }
+        
+        this.grado = exp;
+        this.coeficientes = nuevoArreglo;
+    }
+    
+    if(this.coeficientes[exp] != 0){
+       this.coeficientes[exp] = coef;
+    }
+    
+    
+}
+    
+    
+    public Polinomio[] dividir(Polinomio p){
+        Polinomio[] resultados = new Polinomio[2];
+        boolean continuar= false;
+        
+        for(int i= p.grado; i>=0;i--){
+            if(p.coeficientes[i] != 0 ){
+                continuar = true;
+            }
+        }
+        
+        if(continuar == true){
+        resultados[0]= new Polinomio();
+        resultados[1]= new Polinomio();
+        
+        //resultados[0] = Cociente de la division
+        resultados[0].coeficientes = new double[this.grado - p.grado +1];
+         //resultados[1] = Resto de la division
+        resultados[1].coeficientes = this.coeficientes.clone();
+        resultados[1].grado = this.grado;
+        
+        //Si el grado del Dividendo es menor que el grado del divisor -> C(x)= 0 y R(x)= D(x).
+        if(this.grado < p.grado){ 
+            return resultados;
+        }
+         
+        int gradoCociente = this.grado - p.grado; 
+        
+        resultados[0].grado = gradoCociente;
+          
+        for(int i= gradoCociente; i >= 0 ; i--){
+            resultados[0].coeficientes[i] = resultados[1].coeficientes[i + p.grado]/p.coeficientes[p.grado];
+            
+            for(int j=0; j<= p.grado; j++){
+                resultados[1].coeficientes[i + j] -= resultados[0].coeficientes[i] * p.coeficientes[j];
+            }
+        }
+      }
+        System.out.println("Indefinido");
+        return resultados;
+    }
+    
+  
+    
+   
+    public double evaluar(double x){
+        double[] aux;//= new double[this.grado +1];
+        double res;
+        
+        aux = this.coeficientes;
+        for(int i= this.grado-1; i>=0;i--){
+            aux[i] += x* aux[i + 1]; 
+        }
+        res = aux[0];
+        
+        return res;
+    }
+    
+     
+    public Polinomio derivar(){
+        Polinomio resultado= new Polinomio();
+        
+        resultado.grado = this.grado-1;
+        resultado.coeficientes = new double[resultado.grado +1];
+        
+        for(int i= this.grado; i>0 ;i--){
+            resultado.coeficientes[i-1] = this.coeficientes[i];
+        }
+        
+        for(int i= resultado.grado; i>=0 ;i--){
+            resultado.coeficientes[i] *= (i + 1); 
+        }
+        return resultado;
+    }
+    
+    
+    //+ integrar(): Polinomio
+    public Polinomio integrar(){
+        Polinomio resultado = new Polinomio();
+        
+        resultado.grado = this.grado +1;
+        resultado.coeficientes = new double [resultado.grado +1];
+        
+        resultado.coeficientes[0]= 0;
+        //resultado.coeficientes= this.coeficientes;
+   
+        for(int i=this.grado; i>=0; i-- ){
+            resultado.coeficientes[i+1] = this.coeficientes[i] / (i+1);
+        }
+         
+        return resultado;
+    }
+    
+    
+    //+ calcularRaices(): double[]
+    public double[] calcularRaices(){
+        double[] raices = new double[2];
+        
+        if(this.grado == 1 ){
+            raices[0] = (-1) *this.coeficientes[0] / this.coeficientes[1];
+            return raices;
+        }
+        
+        if(this.grado == 2){
+            double a= this.coeficientes[2];
+            double b= this.coeficientes[1];
+            double c= this.coeficientes[0];
+            
+            //System.out.println( "A= " + a);
+            //System.out.println( "B= " + b);
+            //System.out.println( "C= " + c);
+            
+            double discriminante= b*b - 4*a*c;
+            //System.out.println( "Discriminante= " + discriminante);
+            
+            if(discriminante >0){
+                raices[0] = (-1*b + Math.sqrt(discriminante))/(2*a); 
+                raices[1] = (-1*b - Math.sqrt(discriminante))/(2*a);
+            } else {
+                if(discriminante == 0){
+                    raices[0] = -1*b /2*a;
+                    raices[1] = -1*b /2*a;
+                }else {
+                    System.out.println("Raices imaginarias: ");
+                  //  raices[0] = (((-1)*b)/2*a) + (((-1)*Math.sqrt(discriminante)) /2*a);
+                  //  raices[1] = (((-1)*b)/2*a) - (((-1)*Math.sqrt(discriminante)) /2*a);
+                  
+                }
+            }         
+        }
+        
+       if(raices != null){
+           return raices;
+       } 
+        return null;
+    }
+    
+    
+    
+    public boolean equals(Object obj){
+        if(this == obj){
+            return true;
+        }
+        if(obj == null){
+            return false;
+        }
+        if(getClass() != obj.getClass()){
+            return false;
+        }
+        
+        final Polinomio other = (Polinomio) obj;
+        if(this.grado != other.grado){
+            return false;
+        }
+        if(this.coeficientes != other.coeficientes){
+            return false;
+        }
+        
+        return Arrays.equals (this.coeficientes, other.coeficientes);
+        
+    }
+    
+    
+
+    public String toString() {
+     // 1. Preparamos la "caja" vacía
+    StringBuilder caja = new StringBuilder();
+
+    for (int i = coeficientes.length - 1; i >= 0; i--) {
+        double coef = coeficientes[i];
+
+        if (coef != 0) {
+            
+            if (coef > 0 && caja.length() > 0) {
+                caja.append(" + "); 
+            } else if (coef < 0) {
+                caja.append(" - ");
+            }
+
+             double valorAbs = Math.abs(coef);
+            
+            if (valorAbs == (long) valorAbs) {
+                
+                caja.append((long) valorAbs);
+            } else {
+                
+                caja.append(valorAbs);
+            }
+            
+            
+            if (i > 1) {
+                caja.append("x^");
+                caja.append(i);
+                caja.append(" ");
+            }
+            if(i == 1 ) caja.append("x");
+        }
+    }
+   
+    return caja.toString();
+    }
+    
+    
+    //+ static sumar(p1: Polinomio, p2: Polinomio): Polinomio
+    public static Polinomio sumar(Polinomio p1, Polinomio p2){
+        Polinomio suma = new Polinomio();
+        
+        suma.grado= p1.grado;
+        suma.coeficientes = p1.coeficientes;
+         suma.sumar(p2);
+       
+        
+        return suma;
+    }
+    
+    
+    
+    //+ static restar(p1: Polinomio, p2: Polinomio): Polinomio
+    public static Polinomio restar(Polinomio p1, Polinomio p2){
+         Polinomio resta = new Polinomio();
+        
+        resta.grado = p1.grado;
+        resta.coeficientes = p1.coeficientes;
+        for(int i=p2.grado; i >=0; i--){
+           p2.coeficientes[i] *= -1; 
+        }
+        
+        resta.sumar(p2);
+ 
+        return resta;
+    }
+    
+    
+    
+    //+ static multiplicar(p1: Polinomio, p2: Polinomio): Polinomio
+    public static Polinomio multiplicar(Polinomio p1, Polinomio p2){
+        Polinomio producto = new Polinomio();
+        
+        producto.grado = p1.grado + p2.grado;
+        producto.coeficientes = new double[producto.grado +1];
+        
+        for(int i =0; i<= p1.grado; i++){
+            for(int j=0; j<= p2.grado; j++){
+                producto.coeficientes[i + j] += p1.coeficientes[i] * p2.coeficientes[j]; 
+            }
+        }
+        
+        return producto;
+    }
+    
+}
---
+++ TP2_EJERCICIO_4/src/tp2_ejercicio_4/TP2_EJERCICIO_4.java
@@ -0,0 +1,130 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package tp2_ejercicio_4;
+import java.util.Arrays;
+/**
+ *
+ * @author Pc
+ */
+//import java.util.Arrays;
+public class TP2_EJERCICIO_4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Los coeficientes se cargan de atras para adelante
+        double[] listaCoef1 = {7,-10,2,4,3}; 
+        double[] listaCoef2 = {2,-5,3};
+        double[] listaCoef3 = {-4,3,-3,0,2};
+        double[] listaCoef4 = {2,-1};
+        
+        //Ingresar Polinomio
+        //Polinomio p1= new Polinomio();
+        //p1.ingresarPolinomio();
+        //System.out.println(p1.toString());
+        
+        
+        //Agregar Coeficiente
+        //p1.agregarCoeficiente(-10,5);
+        //System.out.println(p1.toString());
+        
+        
+        Polinomio miPoli1 = new Polinomio(listaCoef1);
+        miPoli1.setGrado(4);
+        Polinomio miPoli2 = new Polinomio(listaCoef2);
+        miPoli2.setGrado(2);
+        Polinomio miPoli3 = new Polinomio(listaCoef3);
+        miPoli3.setGrado(4);
+        Polinomio miPoli4 = new Polinomio(listaCoef4);
+        miPoli4.setGrado(1);
+       
+        //GET COEFICIENTE
+        System.out.println(miPoli1.getCoeficiente(1));
+       
+       //GET COEFICIENTES
+       //double[] aux = miPoli1.getCoeficientes();
+       System.out.println(Arrays.toString(miPoli1.getCoeficientes()));
+       
+       
+       
+        //Suma de Polinomios (operacion de instancia)
+        miPoli1.sumar(miPoli2);
+        System.out.println("Resultado de la suma de Polinomio1 y Polinomio2: ");
+        System.out.println(miPoli1.toString());
+       /* */
+        
+        miPoli3.restar(miPoli4);
+        System.out.println("Resultado de la resta de Polinomio3 y Polinomio4: ");
+        System.out.println(miPoli2.toString());
+        
+        
+       
+        //GETTER
+        System.out.println(miPoli1.getGrado());
+        
+        //SETTER
+        miPoli1.setGrado(3);
+        System.out.println("Nuevo grado de Polinomio 1: " );
+        System.out.println(miPoli1.getGrado());
+        System.out.println(miPoli1.toString());
+        
+       
+       //Division de Polinomios
+       System.out.println("Division: ");
+       Polinomio[] resultado = miPoli1.dividir(miPoli2);
+       Polinomio cociente= resultado[0];
+       Polinomio resto= resultado[1];
+       
+       System.out.println("Resultados de la Division");
+       System.out.print("COCIENTE: ");
+       System.out.println(cociente.toString());
+       System.out.print("RESTO: ");
+       System.out.println(resto.toString());
+        
+       //Multiplicacion
+        System.out.println("Multiplicacion: ");
+        Polinomio prod;
+        prod = Polinomio.multiplicar(miPoli2, miPoli4);
+        System.out.println(prod.toString());
+       
+       
+      //Evaluar un polinomio en un punto
+      System.out.println("Resultado de evaluar en polinomio en un punto: ");
+      System.out.println(miPoli3.evaluar(-2));
+        
+       
+      //Derivar
+      System.out.println("Resultado Derivar: ");
+      System.out.println(miPoli3.derivar().toString());
+        
+        
+        
+    //Prueba integrar un Polinomio
+    System.out.println("Integral: ");
+    System.out.println(miPoli3.integrar().toString());
+    
+    
+    //Prueba calcular raices
+    System.out.println("Raices: ");
+    double[] raices;// = new double[2];
+     
+    raices = miPoli2.calcularRaices();
+    
+    System.out.println(raices[0]);
+    System.out.println(raices[1]);
+     /**/
+       
+     
+     //EQUALS
+     System.out.println(miPoli1.equals(miPoli2));
+         
+    }
+
+}

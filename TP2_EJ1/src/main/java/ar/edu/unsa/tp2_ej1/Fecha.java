c7eb1382dd83300da2bfb2bcc1672078cd1ed8f9
jdfesa <asefadoj@gmail.com>
2026-04-27 17:02:20 -0300

    WIP: Implementación inicial de clase Fecha - Atributos, Constructores y Accessors (TP2_EJ1)

--- TP2_EJ1/src/main/java/ar/edu/unsa/tp2_ej1/Fecha.java
+++ TP2_EJ1/src/main/java/ar/edu/unsa/tp2_ej1/Fecha.java
@@ -1,4 +1,3 @@
-
 package ar.edu.unsa.tp2_ej1;
 
 /**
@@ -10,4 +9,43 @@ public class Fecha {
     private int mes;
     private int anio;
     
+    public Fecha(){
+        dia = 1;
+        mes = 1;
+        anio = 1990;
+    }
+    
+    public Fecha(int dia, int mes, int anio){
+        this.dia = dia;
+        this.mes = mes;
+        this.anio = anio;
+    }
+    
+    public int getDia(){
+        return dia;
+    }
+    
+    public void setDia(int d){
+        this.dia = d;
+    }
+    
+    public int getMes(){
+        return mes;
+    }
+    
+    public void setMes(int m){
+        this.mes = m;
+    }
+    
+    public int getAnio(){
+        return anio;
+    }
+    
+    public void setAnio(int a){
+        this.anio = a;
+    }
+    
+    
+    
 }
+

4fd9953b1194f609778be294f362ddb7e0df6750
alexisC <vancolialexis9@gmail.com>
2026-04-28 01:16:51 -0300

    Fecha 1.0

--- TP_2_1-5/src/Fecha.java
+++ TP_2_1-5/src/Fecha.java
@@ -15,6 +15,7 @@ public class Fecha {
         this.dia = dia;
         this.mes = mes;
         this.anio = anio;
+        normalizar();
     }
 
     public int getDia() {
@@ -31,50 +32,146 @@ public class Fecha {
 
     public void setDia(int dia) {
         this.dia = dia;
+        normalizar();
     }
 
     public void setMes(int mes) {
         this.mes = mes;
+        normalizar();
     }
 
     public void setAnio(int anio) {
         this.anio = anio;
+        normalizar();
     }
 
-    private static int meses30(int dia){
-        if(dia <= 30 && dia > 0){
-            return dia;
-        }else{
-            return dia - (dia % 30);
-        }
-    }
-
-    private static int meses31(int dia){
-        if(dia <= 31 && dia > 0){
-            return dia;
-        }else{
-            return dia - (dia % 31);
-        }
-    }
-
-    private static int meses(int dia, int mes, int anio){
-
-    }
-    private void normaliza(){
-        switch (mes){
-            case 1:
-                this.dia = meses31(dia);
-            case 2:
-                //this.dia = dia - meses31(dia);
-            case 3:
-                this.dia = meses31(dia);
-            case 4:
-                this.dia = meses30(dia);
-        }
+    public void imprimir(){
+        System.out.println(this.dia + " / " + this.mes + " / " + this.anio);
     }
 
     public static boolean esBisiesto(int anio){
+        if((anio % 400 == 0) || (anio % 4 == 0)){
+            return true;
+        }
+        if(anio % 100 == 0){
+            return false;
+        }
+        return  false;
+    }
 
-        return false;
+    private int diasMes(int mes){
+        switch (mes){
+            case 1: return 31;
+            case 2:
+                if(esBisiesto(this.anio)){
+                    return 29;
+                }else{
+                    return 28;
+                }
+            case 3: return 31;
+            case 4: return 30;
+            case 5: return 31;
+            case 6:return 30;
+            case 7:return 31;
+            case 8:return 31;
+            case 9:return 30;
+            case 10:return 31;
+            case 11:return 30;
+            case 12: return 31;
+            default:return 0;
+        }
+    }
+
+    private boolean valida(){
+        if(mes < 1 || mes > 12){
+            return false;
+        }
+        if(dia < 1){
+            return false;
+        }
+        int maximoDias = diasMes(mes);
+        if(dia <= maximoDias){
+            return true;
+        }
+        if (dia > maximoDias) {
+            return false;
+        }else{
+            return false;
+        }
+    }
+
+    private void normalizar(){
+        while(mes < 1){
+            mes += 12;
+            anio--;
+        }
+        while(mes > 12){
+            mes -= 12;
+            anio++;
+        }
+
+        while(dia < 1){
+            mes = mes - 1;
+            if(mes == 0){
+                mes = 12;
+                anio--;
+            }
+            dia = dia + diasMes(mes);
+        }
+
+        while(dia > diasMes(mes)){
+            dia = dia - diasMes(mes);
+            mes++;
+            if(mes == 13){
+                mes = 1;
+                anio++;
+            }
+        }
+    }
+
+    public void adelantar(int d){
+        dia = dia + d;
+        normalizar();
+    }
+
+    public void reiniciar(int d, int m, int a){
+        this.dia = d;
+        this.mes = m;
+        this.anio = a;
+        normalizar();
+    }
+
+    public int comparar(Fecha otra){
+        if(this.anio == otra.anio){
+            if(this.mes == otra.mes){
+                if(this.dia == otra.dia){
+                    return 0;
+                }
+                if(this.dia > otra.dia){
+                    return 1;
+                }else{
+                    return -1;
+                }
+            }
+            if(this.mes > otra.mes){
+                return 1;
+            }else{
+                return -1;
+            }
+        }
+        if(this.anio > otra.anio){
+            return 1;
+        }else{
+            return -1;
+        }
+    }
+
+    public Fecha clonar(){
+        Fecha nuevo = new Fecha(this.dia,this.mes,this.anio);
+        return nuevo;
+    }
+
+    public int diferenciaEnDias(Fecha otra){
+
     }
 }
